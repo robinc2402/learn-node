04362249a7881ee28467950846a80f7d3f4a8ed6
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-08-29 11:32:14 +0530

    1st commit - testing importing built-in modules of NodeJs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-var http = require("http");
-
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const fs = require("fs");
+fs.appendFile("notes.txt", "\nThis text file was created by NodeJS;", () => {
+  console.log("1");
+});
