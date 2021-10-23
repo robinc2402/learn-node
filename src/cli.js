70e4f627de1377c8a33d754feb6fc46118f2905c
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-10-23 18:54:03 +0530

    playing with CLI inpuit

--- src/cli.js
+++ src/cli.js
@@ -1,2 +1,8 @@
 // receiving command line params
-console.log(process.argv);
+//console.log(process.argv[2]);
+
+const input = process.argv[2];
+
+if ("something" === input) {
+  console.log("Recieved something.");
+}
