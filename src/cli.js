2c5938394b5e9ae824fd42e40796a78fe37efcd6
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-10-23 19:06:34 +0530

    Basic usage of yargs Command line parser

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,12 @@
 // receiving command line params
 //console.log(process.argv[2]);
+const yargs = require("yargs");
+// customising yargs version
+yargs.version("0.1.0");
+const inputs = yargs.argv;
 
-const input = process.argv[2];
-
-if ("something" === input) {
+if ("something" === inputs.title) {
   console.log("Recieved something.");
 }
+
+console.log(inputs);
