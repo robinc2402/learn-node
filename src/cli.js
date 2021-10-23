ef3817d43b023b2f77a7520a1ebd5185817a0992
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-10-23 19:20:41 +0530

    Basic Command functions created using Yargs

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,40 @@
 // receiving command line params
 //console.log(process.argv[2]);
 const yargs = require("yargs");
+const notes = require("./notes-app/notes.js");
 // customising yargs version
 yargs.version("0.1.0");
-const inputs = yargs.argv;
 
-if ("something" === inputs.title) {
-  console.log("Recieved something.");
-}
+// if ("something" === inputs.title) {
+//   console.log("Recieved something.");
+// }
 
-console.log(inputs);
+//create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("adding a new note");
+  }
+});
+
+//create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a new note",
+  handler: function () {
+    console.log("removing a new note");
+  }
+});
+
+//create list command
+yargs.command({
+  command: "list",
+  describe: "List all notes",
+  handler: function () {
+    console.log("Listing all notes");
+    console.log(notes);
+  }
+});
+
+console.log(yargs.argv);
