336f65097e3012558b903a6765bf408020eebad3
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-10-23 21:43:51 +0530

    pulling changes

--- src/notes-app/notes.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-const buffer = new Buffer.from(fs.readFileSync("./notes.txt", "utf8"));
-const notesText = buffer.toString();
-//console.log(buffer.toString());
-
-module.exports = notesText;
--- src/notes-app/utils.js
+++
@@ -1,7 +0,0 @@
-const name = "Robin Chalia";
-
-const add = function (a, b) {
-  return a + b;
-};
-
-module.exports = add;
