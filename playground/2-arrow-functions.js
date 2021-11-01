eff376bf231f365c2b222b0717dd5f12be072927
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-11-01 23:03:20 +0530

    Arrow function various examples - arrow functions - arrow functions shorthand - function as a property of an object - examples to understand usage of "this" keyword inside object

---
+++ playground/2-arrow-functions.js
@@ -0,0 +1,50 @@
+// general/conventional function
+const square = function (x) {
+  console.log(x * x);
+};
+
+// arrow function
+const square2 = (x) => {
+  console.log(x * x);
+};
+
+// arrow function shorthand
+const square3 = (x) => console.log(x * x);
+
+square3(15);
+
+// understanding where to use conventional function or arrow function
+// 1 basic difference is that arrow function doesn't use the reference of "this" keyword of its parent object
+
+// Example1 -- conventional function as a property of an object
+const event = {
+  name: "Service anniversary",
+  guestList: ["AkshatT", "AnkitT", "DeepakR", "DineshK"],
+  printGuestList: function () {
+    console.log("We are going to this party tonight --> " + this.name);
+  }
+};
+//event.printGuestList();
+
+// Example2 -- arrow function as a property of an object
+const event2 = {
+  name: "Service anniversary",
+  guestList: ["AkshatT", "AnkitT", "DeepakR", "DineshK"],
+  printGuestList: () => {
+    console.log("We are going to this party tonight --> " + this.name);
+  }
+};
+// event2.printGuestList();
+
+// Example3 -- real function as a property of an object
+const event3 = {
+  name: "Service anniversary",
+  guestList: ["AkshatT", "AnkitT", "DeepakR", "DineshK"],
+  printGuestList() {
+    console.log("We are going to this party tonight --> " + this.name);
+    this.guestList.forEach(function (v) {
+      console.log(v + " is attending the party!");
+    });
+  }
+};
+event3.printGuestList();
