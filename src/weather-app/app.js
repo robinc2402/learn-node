bd3eaca4b6200a4671f7a02635069f66c48e3edd
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-11-03 18:04:30 +0530

    Minor - corrected the output

--- src/weather-app/app.js
+++ src/weather-app/app.js
@@ -4,7 +4,9 @@ const mb_url =
   "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoicm9iaW5jMjQwMiIsImEiOiJja3ZqZmtzdDM4cmd6Mm9wZ24wbGJ2cG55In0.Y_bWsQlBB_ptW76hUni5zw";
 
 request({ url: mb_url, json: true }, (err, res) => {
-  console.log(res.body.features[0].center);
+  const lat = res.body.features[0].center[1];
+  const long = res.body.features[0].center[0];
+  console.log(lat, long);
 });
 
 // const url =
