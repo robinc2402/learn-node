de2563c6227b4bbdb3d0b80f1081dcec8251bdcd
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-11-03 15:54:20 +0530

    Taking out important values out of API json

--- src/weather-app/app.js
+++ src/weather-app/app.js
@@ -3,7 +3,17 @@ const request = require("postman-request");
 const url =
   "http://api.weatherstack.com/current?access_key=8711759fff4b4c8e4743b744d0e7217c&query=37.8267,-122.4233";
 
-request({ url: url }, (err, res) => {
-  const data = res.body;
-  console.log(JSON.parse(data));
+request({ url: url, json: true }, (err, res) => {
+  console.log(res);
+
+  console.log(
+    res.body.current.weather_descriptions[0] +
+      ". It is currently " +
+      res.body.current.temperature +
+      "degrees. It feels like " +
+      res.body.current.feelslike +
+      " degrees out. There is a " +
+      res.body.current.precip +
+      "% chance of rain."
+  );
 });
