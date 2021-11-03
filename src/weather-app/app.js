ec2bda99f1bd8bbbf6cd7604b378ae1c05f9d215
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-11-03 18:01:19 +0530

    HTTP example - fetched Latitude and Longitude from MapBox APIs

--- src/weather-app/app.js
+++ src/weather-app/app.js
@@ -1,19 +1,26 @@
 const request = require("postman-request");
 
-const url =
-  "http://api.weatherstack.com/current?access_key=8711759fff4b4c8e4743b744d0e7217c&query=37.8267,-122.4233";
+const mb_url =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoicm9iaW5jMjQwMiIsImEiOiJja3ZqZmtzdDM4cmd6Mm9wZ24wbGJ2cG55In0.Y_bWsQlBB_ptW76hUni5zw";
 
-request({ url: url, json: true }, (err, res) => {
-  console.log(res);
-
-  console.log(
-    res.body.current.weather_descriptions[0] +
-      ". It is currently " +
-      res.body.current.temperature +
-      "degrees. It feels like " +
-      res.body.current.feelslike +
-      " degrees out. There is a " +
-      res.body.current.precip +
-      "% chance of rain."
-  );
+request({ url: mb_url, json: true }, (err, res) => {
+  console.log(res.body.features[0].center);
 });
+
+// const url =
+//   "http://api.weatherstack.com/current?access_key=8711759fff4b4c8e4743b744d0e7217c&query=37.8267,-122.4233";
+
+// request({ url: url, json: true }, (err, res) => {
+//   console.log(res);
+
+//   console.log(
+//     res.body.current.weather_descriptions[0] +
+//       ". It is currently " +
+//       res.body.current.temperature +
+//       "degrees. It feels like " +
+//       res.body.current.feelslike +
+//       " degrees out. There is a " +
+//       res.body.current.precip +
+//       "% chance of rain."
+//   );
+// });
