48472f2391269b5476b1999cc8502e42122df5c0
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-11-05 22:24:42 +0530

    Practicing callbacks - divided into modules

--- src/weather-app/app.js
+++ src/weather-app/app.js
@@ -1,23 +1,6 @@
 const request = require("postman-request");
+const get_coords = require("./geocode.js");
 const chalk = require("chalk");
-
-// get lat-long for given location
-const get_coords = (location, cb) => {
-  const mb_url =
-    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
-    encodeURIComponent(location) +
-    ".json?access_token=pk.eyJ1Ijoicm9iaW5jMjQwMiIsImEiOiJja3ZqZmtzdDM4cmd6Mm9wZ24wbGJ2cG55In0.Y_bWsQlBB_ptW76hUni5zw";
-
-  request({ url: mb_url, json: true }, (err, res) => {
-    if (err || res === "undefined") {
-      cb("Some error occured!", res);
-    } else if (!("features" in res.body) || !res.body.features.length) {
-      cb("Unabled to find location!", res);
-    }
-
-    cb(undefined, res);
-  });
-};
 
 // get weather report for provided location
 const weather_report = (location, cb) => {
