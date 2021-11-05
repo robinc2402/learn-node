8005fe975c2aea5a336ad61aae03bf91df64bdd9
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-11-05 23:33:06 +0530

    Object shorthand and destructuring

---
+++ src/playground/5-es6-objects.js
@@ -0,0 +1,27 @@
+// Object property shorthand
+
+const name = "Robin";
+const age = "32";
+const user = {
+  name,
+  age,
+  location: "Gurugram"
+};
+
+console.log(user);
+
+// object destructuring
+
+const product = {
+  label: "red",
+  price: 3,
+  stock: 201,
+  salePrice: undefined,
+  rating: 3.2
+};
+
+const { label: pLabel, stock, rating = 10 } = product;
+
+console.log(pLabel);
+console.log(stock);
+console.log(rating);
