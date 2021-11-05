3cdb2849d641654ecc6da1e80b8ad9e0b8ea3921
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-11-05 22:22:49 +0530

    Practicing callbacks - better error handling

--- src/weather-app/app.js
+++ src/weather-app/app.js
@@ -10,20 +10,18 @@ const get_coords = (location, cb) => {
 
   request({ url: mb_url, json: true }, (err, res) => {
     if (err || res === "undefined") {
-      console.log(chalk.red.inverse("Some error occured!"));
-      return false;
+      cb("Some error occured!", res);
     } else if (!("features" in res.body) || !res.body.features.length) {
-      console.log(chalk.red.inverse("Unabled to find location!"));
-      return false;
+      cb("Unabled to find location!", res);
     }
 
-    cb(res);
+    cb(undefined, res);
   });
 };
 
 // get weather report for provided location
 const weather_report = (location, cb) => {
-  get_coords(location, (res) => {
+  get_coords(location, (err, res) => {
     const lat = res.body.features[0].center[1];
     const long = res.body.features[0].center[0];
 
@@ -35,34 +33,34 @@ const weather_report = (location, cb) => {
 
     request({ url: url, json: true }, (err, res) => {
       if (err) {
-        console.log(chalk.red.inverse("Error connecting weather service!"));
-        return false;
+        cb("Error connecting weather service!", res);
       } else if (res.body.error) {
-        console.log(
-          chalk.red.inverse("Unable to find location! " + res.body.error.info)
-        );
-        return false;
+        cb("Unable to find location! " + res.body.error.info, res);
       }
-      cb(res);
+      cb(undefined, res);
     });
   });
 };
 
-weather_report("Gurugram", (res) => {
-  console.log(
-    chalk.green.inverse(
-      "== Weather report for " + res.body.location.name + " =="
-    )
-  );
+weather_report("Gurugram", (err, res) => {
+  if (err !== undefined) {
+    chalk.red.inverse(err);
+  } else {
+    console.log(
+      chalk.green.inverse(
+        "== Weather report for " + res.body.location.name + " =="
+      )
+    );
 
-  console.log(
-    res.body.current.weather_descriptions[0] +
-      ". It is currently " +
-      res.body.current.temperature +
-      " degrees. It feels like " +
-      res.body.current.feelslike +
-      " degrees out. There is a " +
-      res.body.current.precip +
-      "% chance of rain."
-  );
+    console.log(
+      res.body.current.weather_descriptions[0] +
+        ". It is currently " +
+        res.body.current.temperature +
+        " degrees. It feels like " +
+        res.body.current.feelslike +
+        " degrees out. There is a " +
+        res.body.current.precip +
+        "% chance of rain."
+    );
+  }
 });
