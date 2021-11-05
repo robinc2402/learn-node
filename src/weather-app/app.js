659a86b0c4e29c1056c9c660c5ed942c1b638740
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-11-05 23:19:28 +0530

    Practicing callbacks - Used CLI args to fetch weather data of a place

--- src/weather-app/app.js
+++ src/weather-app/app.js
@@ -3,18 +3,12 @@ const weather_report = require("./utils/geocode.js");
 const forecast = require("./utils/forecast.js");
 const chalk = require("chalk");
 
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
+const location = process.argv[2];
 
-weather_report("Gurugram", (err, res) => {
-  if (err) {
+weather_report(location, (err, res) => {
+  if (!location) {
+    return console.log(chalk.red.inverse("Please provide a location."));
+  } else if (err) {
     return console.log(err);
   }
   forecast(res.body.location.lon, res.body.location.lat, (error, data) => {
