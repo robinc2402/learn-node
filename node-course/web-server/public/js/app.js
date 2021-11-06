c3f459bb525ae4cd01775a6a6592c3765baf3133
Robin Chalia <44494705+robinc2402@users.noreply.github.com>
2021-11-06 22:15:12 +0530

    Express: client side async call. Fetched weather data

--- node-course/web-server/public/js/app.js
+++ node-course/web-server/public/js/app.js
@@ -1,1 +1,11 @@
-console.log("Client side js file is loaded");
+fetch("https://fjdyu.sse.codesandbox.io/weather?address=Gurugram").then(
+  (res) => {
+    res.json().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data);
+      }
+    });
+  }
+);
