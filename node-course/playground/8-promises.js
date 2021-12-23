6a90ed73aa0d69db6d1512776b1b7be7e7df38f1
Robin Chalia <robinc@one.com>
2021-12-23 17:38:36 +0530

    Promise chainging - 2

--- node-course/playground/8-promises.js
+++ node-course/playground/8-promises.js
@@ -6,16 +6,32 @@ const add = (a, b) => {
   });
 };
 
-add(1, 3)
+// add(1, 3)
+//   .then((sum) => {
+//     add(sum, 7)
+//       .then((s) => {
+//         console.log(s);
+//       })
+//       .catch((e) => {
+//         console.log(e);
+//       });
+//     console.log(sum);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+add(1, 1)
   .then((sum) => {
-    add(sum, 7)
-      .then((s) => {
-        console.log(s);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
     console.log(sum);
+    return add(sum, 4);
+  })
+  .then((sum2) => {
+    console.log(sum2);
+    return add(sum2, 4);
+  })
+  .then((sum3) => {
+    console.log(sum3);
   })
   .catch((e) => {
     console.log(e);
